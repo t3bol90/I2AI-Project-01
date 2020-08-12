fde5510fa3b5206257d4d28ff98eed97bd37e828
ductuan15 <tuanduc1506@gmail.com>
2020-08-12 22:25:43 +0700

    fix random_map

--- source/random_map/random_map.py
+++ source/random_map/random_map.py
@@ -1,12 +1,10 @@
 import numpy as np
 import random 
-def random_wall(_map,n_rol,n_col):
-    max_length = 7
-    num_wall = 7
+def random_wall(_map,n_rol,n_col,max_length_wall,num_wall):
     wall_direction = [(0,1),(-1,0),(1,0),(0,-1)]
     for i in range(num_wall):
         wall_index = np.random.randint(0,n_rol * n_col,2)
-        for j in range(max_length):
+        for j in range(max_length_wall):
             wall_index += random.sample(wall_direction,1)[0]
             wall_index[0] %= n_rol
             wall_index[1] %= n_col 
@@ -22,9 +20,9 @@ def random_wall(_map,n_rol,n_col):
                 _map[wall_index[0] % n_rol ,wall_index[1] % n_col] = 1
     return _map
 
-def generate_map(n_rol,n_col,n_food,n_monster):
+def generate_map(n_rol,n_col,n_food,n_monster,max_length_wall,num_wall):
     _map  = np.zeros((n_col,n_rol))
-    _map = random_wall(_map,n_rol,n_col)
+    _map = random_wall(_map,n_rol,n_col,max_length_wall,num_wall)
     # generate food
     for i in range(n_food):
         food_index = np.random.randint(0,n_rol * n_col,2)
@@ -41,11 +39,12 @@ def generate_map(n_rol,n_col,n_food,n_monster):
     return _map,pacman
 
 
-n_rol = 15
-n_col = 15
-n_food = 4
-n_monster = 0
-_map,pacman = generate_map(n_col,n_rol,n_food,n_monster)
+# n_rol = 15
+# n_col = 15
+# n_food = 4
+# n_monster = 0
+n_rol,n_col,n_food,n_monster,max_length_wall,num_wall = list(map(int,input().split()))
+_map,pacman = generate_map(n_col,n_rol,n_food,n_monster,max_length_wall,num_wall)
 h = f'{n_rol} {n_col}'
 f = f'{pacman[0]} {pacman[1]}'
 np.savetxt('map.txt',_map,header = h,footer = f,fmt='%.0f',comments= '')
