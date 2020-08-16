a26c8ba53650f020fc5a11f0612ef3800bf1d7c7
t3bol90 <becute26112000@gmail.com>
2020-08-16 17:50:27 +0700

    Fix retangle

--- source/random_map/random_map.py
+++ source/random_map/random_map.py
@@ -1,28 +1,28 @@
 import numpy as np
 import random 
-def random_wall(_map,n_rol,n_col,max_length_wall,num_wall):
-    wall_direction = [(0,1),(-1,0),(1,0),(0,-1)]
-    for i in range(num_wall):
-        wall_index = np.random.randint(0,n_rol * n_col,2)
-        for j in range(max_length_wall):
-            wall_index += random.sample(wall_direction,1)[0]
-            wall_index[0] %= n_rol
-            wall_index[1] %= n_col 
+# def random_wall(_map,n_rol,n_col,max_length_wall,num_wall):
+#     wall_direction = [(0,1),(-1,0),(1,0),(0,-1)]
+#     for i in range(num_wall):
+#         wall_index = np.random.randint(0,n_rol * n_col,2)
+#         for j in range(max_length_wall):
+#             wall_index += random.sample(wall_direction,1)[0]
+#             wall_index[0] %= n_rol
+#             wall_index[1] %= n_col 
 
-            # check neibor
-            neibor = np.array([wall_index + x for x in wall_direction])
-            count_neibor = 0
+#             # check neibor
+#             neibor = np.array([wall_index + x for x in wall_direction])
+#             count_neibor = 0
 
-            for n in range(4):
-                count_neibor += _map[(neibor[n][0] % n_rol,neibor[n][1] % n_rol)]
+#             for n in range(4):
+#                 count_neibor += _map[(neibor[0][n]] % n_rol,neibor[n][1] % n_rol)]
 
-            if count_neibor <= 1:
-                _map[wall_index[0] % n_rol ,wall_index[1] % n_col] = 1
-    return _map
+#             if count_neibor <= 1:
+#                 _map[wall_index[0] % n_rol ,wall_index[1] % n_col] = 1
+#     return _map
 
 def generate_map(n_rol,n_col,n_food,n_monster,max_length_wall,num_wall):
     _map  = np.zeros((n_col,n_rol))
-    _map = random_wall(_map,n_rol,n_col,max_length_wall,num_wall)
+    # _map = random_wall(_map,n_rol,n_col,max_length_wall,num_wall)
     # generate food
     for i in range(n_food):
         food_index = np.random.randint(0,n_rol * n_col,2)
@@ -39,8 +39,8 @@ def generate_map(n_rol,n_col,n_food,n_monster,max_length_wall,num_wall):
     return _map,pacman
 
 
-n_rol = 15
-n_col = 30
+n_rol = 19
+n_col = 21
 n_food = 1
 n_monster = 0
 max_length_wall = 5
@@ -49,5 +49,5 @@ num_wall = 20
 _map,pacman = generate_map(n_col,n_rol,n_food,n_monster,max_length_wall,num_wall)
 h = f'{n_rol} {n_col}'
 f = f'{pacman[0]} {pacman[1]}'
-np.savetxt('map9.txt',_map,header = h,footer = f,fmt='%.0f',comments= '')
+np.savetxt('retanglemap.txt',_map,header = h,footer = f,fmt='%.0f',comments= '')
 
